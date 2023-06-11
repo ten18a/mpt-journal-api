b60c70a8ce3eee91cf00db8418a88c6f80db1939
ten18a <123708109+ten18a@users.noreply.github.com>
2023-06-11 10:26:18 +0300

    Unstable API.

---
+++ unstable/index.php
@@ -0,0 +1,25 @@
+<?php
+
+// APIv:
+$apiv = 'unstable';
+
+// Define base URL
+$base_url = 'https://api.mpt-journal.ru/unstable/';
+
+// Get request method and URI
+$method = $_SERVER['REQUEST_METHOD'];
+
+$endpoint = $_GET['endpoint'];
+
+// Route the request to the appropriate endpoint
+switch ($endpoint) {
+    case 'apiv':
+        echo "$apiv";
+    break;
+    // Add more cases for other endpoints specific to the unstable-beta version
+    default:
+        // Return a 404 Not Found response for unknown endpoints
+        http_response_code(404);
+        echo json_encode(array('message' => 'Endpoint not found'));
+        break;
+}
