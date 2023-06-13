a95d93a2eb89dcb6ae44ca9b5b4e0d10eec166da
ten18a <123708109+ten18a@users.noreply.github.com>
2023-06-14 00:38:56 +0300

    Update API structure.

--- unstable/index.php
+++ unstable/index.php
@@ -7,19 +7,30 @@ $apiv = 'unstable';
 $base_url = 'https://api.mpt-journal.ru/unstable/';
 
 // Get request method and URI
-$method = $_SERVER['REQUEST_METHOD'];
-
+$method = $_GET['method'];
 $endpoint = $_GET['endpoint'];
 
 // Route the request to the appropriate endpoint
-switch ($endpoint) {
-    case 'apiv':
-        echo json_encode(array('apiv' => $apiv));
+switch ($method) {
+    case 'GET':
+        switch ($endpoint) {
+            case 'apiv':
+                echo json_encode(array('version' => $apiv));
+            break;
+            case 'fromMPT.schedule-changes':
+                include 'GET/fromMPT.schedule-changes.php';
+            break;
+            default:
+                // Return a 404 Not Found response for unknown endpoints
+                http_response_code(404);
+                echo json_encode(array('message' => 'Endpoint not found'));
+            break;
+        }
     break;
-    // Add more cases for other endpoints specific to the unstable-beta version
     default:
         // Return a 404 Not Found response for unknown endpoints
         http_response_code(404);
-        echo json_encode(array('message' => 'Endpoint not found'));
-        break;
+        echo json_encode(array('message' => 'Method not found'));
+    break;
 }
+?>
